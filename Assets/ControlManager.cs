a85b356600a4aefa0f74d18501c9bdc7a2ca0abb
Michael Bolland <michaelbollanduk@gmail.com>
2015-09-15 22:22:35 +0100

    first commit

---
+++ Assets/ControlManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlManager : MonoBehaviour {
+
+    const int orthographicSizeMin = 10;
+    const int orthographicSizeMax = 20;
+
+    private Vector3 startDragPos;
+    private Vector3 endDragPos;
+    private GameObject selectionbox;
+
+    // Use this for initialization
+    void Start() {
+        Camera.main.orthographicSize = 10;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        handleScrollUpdate();
+        handleDragSelect();
+    }
+
+    void handleDragSelect()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if (selectionbox == null)
+            {
+                startDragPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                selectionbox = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                selectionbox.transform.position = new Vector3(startDragPos.x, startDragPos.y, 0);  ;
+                selectionbox.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                selectionbox.GetComponent<Renderer>().material.color = Color.yellow;
+            } else
+            {
+                Vector3 newpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                float x = newpos.x + newpos.x;
+                float y = newpos.y + newpos.y;
+
+                selectionbox.transform.position = new Vector3(newpos.x, newpos.y, 0.1f);
+            }
+            
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            endDragPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Destroy(selectionbox);
+        }
+
+
+    }
+
+    void handleScrollUpdate()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) // forward
+        {
+            Camera.main.orthographicSize++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+        {
+            Camera.main.orthographicSize--;
+        }
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, orthographicSizeMin, orthographicSizeMax);
+    }
+
+}
---
+++ Assets/TileManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+
+    public Transform tilePrefab;
+
+    private int maxrenderx = 128;
+    private int maxrendery = 128;
+
+    private int centerpointx = 0;
+    private int centerpointy = 0;
+
+    // Use this for initialization
+    void Start () {
+        //Load prefab "Grass" from "Resources/Prefabs/" folder.
+        tilePrefab = Resources.Load<Transform>("Tiles/Grass1");
+
+        //If we can't find the prefab then log a warning.
+        if (!tilePrefab)
+            Debug.LogWarning("Unable to find TilePrefab in your Resources folder.");
+
+        // Start at half the minimum size
+
+
+        //Iterate over each future tile positions for x and y
+        for (int y = 0-(maxrendery/2); y < (maxrendery / 2); y++)
+        {
+            for (int x = 0 - (maxrenderx / 2); x < (maxrenderx / 2); x++)
+            {
+                //Instantiate tile prefab at the desired position as a Transform object
+                Transform tile = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity) as Transform;
+                //Set the tiles parent to the GameObject this script is attached to
+                tile.parent = transform;
+            }
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
