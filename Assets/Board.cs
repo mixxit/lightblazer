fe41647ddd5aec8c6ae96c186a75a7209dedc5e1
Michael Bolland <michaelbollanduk@gmail.com>
2015-09-26 22:19:20 +0100

    merge a*

---
+++ Assets/Board.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject cellPrefab;
+    Cell[,] board;
+
+    void Awake()
+    {
+        CreateBoard();
+    }
+
+    void CreateBoard()
+    {
+        board = new Cell[width, height];
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
+            {
+                GameObject cell = GameObject.Instantiate(
+                  cellPrefab,
+                  new Vector3(j, i, 0),
+                  Quaternion.identity) as GameObject;
+                cell.transform.parent = transform;
+                Cell c = cell.GetComponent<Cell>();
+                c.coordinates = new Vector2(j, i);
+                board[j, i] = c;
+            }
+    }
+
+    protected List<Cell> FindPath(Cell origin, Cell goal)
+    {
+        AStar pathFinder = new AStar();
+        pathFinder.FindPath(origin, goal, board, false);
+        return pathFinder.CellsFromPath();
+    }
+}
---
+++ Assets/Cell.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour
+{
+    public Vector2 coordinates { get; set; }
+    public virtual bool IsWalkable()
+    {
+        return true;
+    }
+    public virtual float MovementCost()
+    {
+        return 0;
+    }
+}
